b4bc3557f4bebf2dc00534669b252416210cb49f
Brittany Morris <brittmmendez@gmail.com>
2018-01-17 12:35:55 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,29 @@
 class Show < ActiveRecord::Base
+  def highest_rating
+  #return the highest value in the ratings column
+  end
+
+  def most_popular_show
+    #return the show with the highest rating
+  end
+
+  def lowest_rating
+    #returns the lowest value in the ratings column.
+  end
+
+  def least_popular_show
+    #returns the show with the lowest rating.
+  end
+
+  def ratings_sum
+    #returns the sum of all of the ratings.
+  end
+
+  def popular_shows
+    #returns an array of all of the shows that have a rating greater than 5. hint
+  end
+
+  def shows_by_alphabetical_order
+    #returns an array of all of the shows sorted by alphabetical order according to their names
+  end
 end
